2e057b2cd9add2721a4751b59815bf293cfd5487
robin-bosch <alphaclass@web.de>
2023-10-05 23:35:30 +0200

    Basic project setup

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [currentNumber, setCurrentNumber] = useState(0)
+
+
+
+    return (
+        <main>
+            <section>
+                <h1>Calc</h1>
+                <div></div>
+            </section>
+            <section>
+
+            </section>
+            <section></section>
+        </main>
+    )
+}
+
+export default App
